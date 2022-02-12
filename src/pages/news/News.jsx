43ab73fde0593ffbdb15542ae057b38b3f64d94f
tuong.lau16072002@hcmut.edu.vn <tuong.lau16072002@hcmut.edu.vn>
2022-02-13 06:53:03 +0700

    slider responsive

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -10,87 +10,93 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 const News = () => {
     window.scrollTo(0,0)
   return (
-    <div className='news' style={{paddingTop:"100px"}}>
-        <div className="news__header">
-            <div className="hot__news">
-                <NewsCard 
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                />
-            </div>
-            <div className="related__news">
-                <div className="label">RELATED</div>
-                <div className="item">
-                    <RelatedCard
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                />
-                </div>
-                <div className="item">
-                    <RelatedCard
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                />
-                </div>
-                <div className="item">
-                    <RelatedCard
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                />
-                </div>
-                <div className="item">
-                    <RelatedCard
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                />
-                </div>
-
-            </div>
+    <div className="news" style={{ paddingTop: "100px" }}>
+      <div className="news__header">
+        <div className="hot__news">
+          <NewsCard
+            topic="Technology"
+            date="March 26, 2022"
+            title="Japan’s virus success has puzzled the world. Is its luck running out?"
+          />
         </div>
-        <div className="news__future">
-            <div className="label">Feature News</div>
-            <div className="content">
-                <Swiper
-                modules={[Navigation, Pagination, Scrollbar]}
-                spaceBetween={50}
-                slidesPerView={3}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                autoplay={{delay:3000}}
-                >
-                <SwiperSlide><NewsCard 
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                /></SwiperSlide>
-                <SwiperSlide><NewsCard 
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                /></SwiperSlide>
-                <SwiperSlide><NewsCard 
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                /></SwiperSlide>
-                <SwiperSlide><NewsCard 
-                    topic="Technology"
-                    date = 'March 26, 2022'
-                    title = "Japan’s virus success has puzzled the world. Is its luck running out?"
-                /></SwiperSlide>
-                </Swiper>                
-            </div>
+        <div className="related__news">
+          <div className="label">RELATED</div>
+          <div className="item">
+            <RelatedCard
+              topic="Technology"
+              date="March 26, 2022"
+              title="Japan’s virus success has puzzled the world. Is its luck running out?"
+            />
+          </div>
+          <div className="item">
+            <RelatedCard
+              topic="Technology"
+              date="March 26, 2022"
+              title="Japan’s virus success has puzzled the world. Is its luck running out?"
+            />
+          </div>
+          <div className="item">
+            <RelatedCard
+              topic="Technology"
+              date="March 26, 2022"
+              title="Japan’s virus success has puzzled the world. Is its luck running out?"
+            />
+          </div>
+          <div className="item">
+            <RelatedCard
+              topic="Technology"
+              date="March 26, 2022"
+              title="Japan’s virus success has puzzled the world. Is its luck running out?"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="news__future">
+        <div className="label">Feature News</div>
+        <div className="content">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar]}
+            spaceBetween={50}
+            slidesPerView='auto'
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            autoplay={{ delay: 3000 }}
             
+          >
+            <SwiperSlide>
+              <NewsCard
+                topic="Technology"
+                date="March 26, 2022"
+                title="Japan’s virus success has puzzled the world. Is its luck running out?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NewsCard
+                topic="Technology"
+                date="March 26, 2022"
+                title="Japan’s virus success has puzzled the world. Is its luck running out?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NewsCard
+                topic="Technology"
+                date="March 26, 2022"
+                title="Japan’s virus success has puzzled the world. Is its luck running out?"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <NewsCard
+                topic="Technology"
+                date="March 26, 2022"
+                title="Japan’s virus success has puzzled the world. Is its luck running out?"
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
 const NewsCard = (props) => {
